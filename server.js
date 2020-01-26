edd4134f765b4fc202ad58e2dc9e854aab07d9db
Muhammed Nuhman <nuhman7@outlook.com>
2020-01-26 13:09:37 +0530

    basic express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`running server on port: ${port}`));
